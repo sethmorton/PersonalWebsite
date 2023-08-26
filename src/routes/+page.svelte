5354a75eae51c2bf08b589687478f2909ba77bb0
Seth Morton <sethm@Seths-MacBook-Pro.local>
2023-08-26 15:39:29 -0700

    Initial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { Application } from '@splinetool/runtime';
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+		const canvas = document.getElementById('canvas3d');
+		const app = new Application(canvas);
+		app.load('https://prod.spline.design/15Djxyt0ppBn9MA0/scene.splinecode');
+	});
+</script>
+
+<div class="h-full min-h-screen">
+	<canvas id="canvas3d" />
+</div>
