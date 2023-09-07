c8acb313db7c4f350cc150789ccf1a4b69784d37
Seth Morton <sethm@Seths-MacBook-Pro.local>
2023-09-07 11:22:46 -0400

    :sparkles: added loading screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,25 +2,26 @@
 	import { Application } from '@splinetool/runtime';
 	import { onMount } from 'svelte';
 	import Typewriter from 'svelte-typewriter';
-
-	let linksInterval = false;
-	let headerInterval = false;
+	import { Jumper } from 'svelte-loading-spinners';
+	let loading = false;
 	onMount(async () => {
-
 		const canvas = document.getElementById('canvas3d');
-const app = new Application(canvas);
-app.load('https://prod.spline.design/i1vVSiQte1dh6fwm/scene.splinecode');
-		intervalSet();
+		const app = new Application(canvas);
+		app.load('https://prod.spline.design/i1vVSiQte1dh6fwm/scene.splinecode');
+		loading = true;
+		// intervalSet();
 	});
-	const intervalSet = () => {	
-		// create a set interval that updates a linksInterval variable after 4 seconds
-		// if linksInterval is true, then show the links
-		// if linksInterval is false, then hide the links
-		// setInterval(() => {
-		// 	linksInterval = !linksInterval;
-		// }, 4000);
-	}
+	$: hiddenClass = loading ? 'hidden' : '';
 </script>
+
+	<div class="h-full min-h-screen flex {hiddenClass} ">
+		<div class="justify-center align-middle">
+			<Jumper size="60" color="#FF3E00" unit="px" duration="1s" />
+		</div>
+		
+	</div>
+	
+
 
 <div class="h-full min-h-screen relative">
 	<canvas class="absolute min-h-screen" id="canvas3d" />
@@ -29,22 +30,25 @@ app.load('https://prod.spline.design/i1vVSiQte1dh6fwm/scene.splinecode');
 			<h1 class="text-white text-7xl font-bold">Hello!</h1>
 		</div>
 		<div class="text-center">
-
 			<Typewriter delay={1000} interval={200}>
 				<h1 class="text-white py-2 text-7xl font-bold">I'm Seth</h1>
 			</Typewriter>
-
 		</div>
 	</div>
 	<div class="absolute top-1/2 right-5 transform translate-y-[-50%]">
 		<div class="text-center">
 			<h4 class="text-white py-2 text-2xl font-bold d-inline">
-
 				<Typewriter delay={2800} interval={200}>
-					<a href="https://github.com/sethmorton">Github | </a><a href="https://www.linkedin.com/in/seth-morton-118574242/">LinkedIn | </a><a href="https://drive.google.com/file/d/16NeIU_46GejIkJzxGvwQpczminl3Tp3C/view?usp=drive_link">Resume</a>
+					<a href="https://github.com/sethmorton">Github | </a><a
+						href="https://www.linkedin.com/in/seth-morton-118574242/"
+						>LinkedIn |
+					</a><a
+						href="https://drive.google.com/file/d/16NeIU_46GejIkJzxGvwQpczminl3Tp3C/view?usp=drive_link"
+						>Resume</a
+					>
 				</Typewriter>
 			</h4>
-
 		</div>
 	</div>
 </div>
+
