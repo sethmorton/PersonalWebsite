66d21b3f5a1968eb1e3011b599b8aa58fba3b775
Seth Morton <sethm@Seths-MacBook-Pro.local>
2023-09-22 00:25:54 -0400

    push to git

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,24 +3,29 @@
 	import { onMount } from 'svelte';
 	import Typewriter from 'svelte-typewriter';
 	import { Jumper } from 'svelte-loading-spinners';
-	let loading = false;
+	let loading = true;
 	onMount(async () => {
 		const canvas = document.getElementById('canvas3d');
-		const app = new Application(canvas);
-		app.load('https://prod.spline.design/i1vVSiQte1dh6fwm/scene.splinecode');
-		loading = true;
+		if (canvas !== null) {
+			const app = new Application(canvas);
+			app.load('https://prod.spline.design/i1vVSiQte1dh6fwm/scene.splinecode').then(() => {
+				showScreen();
+				loading = false;
+			});
+		}
 		// intervalSet();
 	});
-	$: hiddenClass = loading ? 'hidden' : '';
+	const showScreen = () => {
+			hiddenClass = 'hidden';
+	}
+	$: hiddenClass = loading ? '' : 'hidden';
 </script>
 
 <div class="h-full min-h-screen flex bg-black items-center justify-center {hiddenClass}">
-    <div>
-        <Jumper size="200" color="white" unit="px" duration="1s" />
-    </div>
+	<div>
+		<Jumper size="200" color="white" unit="px" duration="1s" />
+	</div>
 </div>
-	
-
 
 <div class="h-full min-h-screen relative">
 	<canvas class="absolute min-h-screen" id="canvas3d" />
@@ -29,14 +34,17 @@
 			<h1 class="text-white text-7xl font-bold">Hello!</h1>
 		</div>
 		<div class="text-center">
+			{#if loading == false}
 			<Typewriter delay={1000} interval={200}>
 				<h1 class="text-white py-2 text-7xl font-bold">I'm Seth</h1>
 			</Typewriter>
+			{/if}
 		</div>
 	</div>
 	<div class="absolute top-1/2 right-5 transform translate-y-[-50%]">
-		<div class="text-center">
+		<div class="text-center p-4">
 			<h4 class="text-white py-2 text-2xl font-bold d-inline">
+				{#if loading == false}
 				<Typewriter delay={2800} interval={200}>
 					<a href="https://github.com/sethmorton">Github | </a><a
 						href="https://www.linkedin.com/in/seth-morton-118574242/"
@@ -46,8 +54,8 @@
 						>Resume</a
 					>
 				</Typewriter>
+				{/if}
 			</h4>
 		</div>
 	</div>
 </div>
-
