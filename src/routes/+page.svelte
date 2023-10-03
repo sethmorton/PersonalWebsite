61eac974b9b948ad46656d3416cdc52d3061b2ae
Seth Morton <sethm@Seths-MacBook-Pro.local>
2023-10-02 23:33:32 -0400

    updated resume

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -50,7 +50,7 @@
 						href="https://www.linkedin.com/in/seth-morton-118574242/"
 						>LinkedIn |
 					</a><a
-						href="https://drive.google.com/file/d/1xjJAgphcj4ctzFXnw0J41_AWexcWqYg3/view?usp=sharing"
+						href="https://drive.google.com/file/d/1mM4cYNtaboo-0MOkQpJ2jQ7TrZR4JsJk/view?usp=sharing"
 						>Resume</a
 					>
 				</Typewriter>
